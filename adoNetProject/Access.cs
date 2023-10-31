725eccd0b5326afc841040d2e81f19242da14384
326659356 <yael05485@gmail.com>
2023-10-31 19:38:29 +0200

    Add project files.

---
+++ adoNetProject/Access.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adoNetProject
+{
+    public class Access
+    {
+
+        public  void InsertDate(string IqueryString, string connectionString)
+        {
+            string ProductName, Description,image,price, CategoryId;
+       
+            string next = "y";
+            while (next == "y")
+            {
+                Console.WriteLine("insert productName: ");
+                ProductName = Console.ReadLine();
+                Console.WriteLine("insert Description: ");
+                Description = Console.ReadLine();
+                Console.WriteLine("insert CategoryId: ");
+                CategoryId =  Console.ReadLine();
+                Console.WriteLine("insert image: ");
+                image = Console.ReadLine();
+                Console.WriteLine("insert price: ");
+                price = Console.ReadLine();
+                Console.WriteLine("do you want to insert more product? (y/n)");
+                next = Console.ReadLine();
+               //try
+               // {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        SqlCommand command = new SqlCommand(IqueryString, connection);
+                        command.Parameters.Add("@ProductName", SqlDbType.VarChar, 20).Value = ProductName;
+                        command.Parameters.Add("@Description", SqlDbType.VarChar, 40).Value = Description;
+                        command.Parameters.Add("@CategoryId", SqlDbType.VarChar, 10).Value = CategoryId;
+                    command.Parameters.Add("@price", SqlDbType.VarChar, 10).Value = price;
+                    command.Parameters.Add("@image", SqlDbType.VarChar, 10).Value = image;
+
+                    command.Connection.Open();
+                        int x = command.ExecuteNonQuery();
+                        Console.WriteLine(x);
+                    }
+               // }
+                //catch (er)
+                //{
+                //    Console.WriteLine(er);
+                //}
+
+            }
+
+           
+
+        }
+        public  void ReadData(string RqueryString, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(RqueryString, connection);
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetInt32(3), reader.GetInt32(4),reader.GetString(5));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no data founded");
+                }
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ adoNetProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace adoNetProject
+{
+    public class Program
+    {
+
+        
+
+       
+        static void Main(string[] args)
+        {
+
+
+            Access ac = new Access();
+            Console.WriteLine("hello");
+            string connectionString = "Data Source=SRV2\\PUPILS;Initial Catalog = Store_326659356; Integrated Security = True";
+            
+            string RqueryString = "select ProductId,ProductName, Description from Products_tbl";
+            string IqueryString = "insert into Products_tbl (ProductName,Description,CategoryId) values (@ProductName,@Description,@CategoryId)";
+
+            //ac.InsertDate(IqueryString, connectionString);
+            ac.ReadData(RqueryString, connectionString);
+
+
+        }
+
+    }
+}
